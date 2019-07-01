1d82c501d7c2dc2746ee4e85b9b843c320aee97d
Andrea Jurado <avjh_18@hotmail.com>
2019-07-01 09:37:48 -0500

    Run - personaje

---
+++ Assets/caballeroPlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class caballeroPlayer : MonoBehaviour
+{
+    Rigidbody2D caballeroRB;
+    public float maxVelocidad;
+    Animator caballeroAnimate;
+    // Start is called before the first frame update
+
+    //rotarCaballero
+    bool  voltearCaballero = true;
+    SpriteRenderer caballeroRender;
+
+    void Start()
+    {
+        caballeroRB = GetComponent<Rigidbody2D> ();
+        caballeroRender = GetComponent<SpriteRenderer> ();
+        caballeroAnimate = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mover = Input.GetAxis ("Horizontal");
+
+        if (mover > 0 && !voltearCaballero){
+            Voltear();
+
+        }else if(mover < 0 && voltearCaballero)
+        {
+            Voltear();
+        }
+        caballeroRB.velocity = new Vector2 (mover * maxVelocidad, caballeroRB.velocity.y);
+        //correr caballero
+        caballeroAnimate.SetFloat("VelMovimiento",Mathf.Abs(mover));
+    }
+
+    void Voltear(){
+        voltearCaballero =!voltearCaballero;
+        caballeroRender.flipX = !caballeroRender.flipX;
+    }
+}
